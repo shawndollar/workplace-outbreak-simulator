97cca021f75a53bf2ec37e7351592602996ad45c
shawndollar <shawn.dollar@gmail.com>
2020-06-19 03:10:24 -0500

    Added toastr popup on completion of simulator.

--- WorkplaceOutbreakSimulatorWebApp/wwwroot/js/simulator.index.js
+++ WorkplaceOutbreakSimulatorWebApp/wwwroot/js/simulator.index.js
@@ -1,7 +1,16 @@
 ﻿// simulator/index js
 $(document).ready(function () {
+
+    displaySimulatorCompleted();
     
 });
+
+function displaySimulatorCompleted() {
+    var el = document.getElementById("SimulatorData.IsSimulatorComplete");
+    if (el !== null && el.value == "true") {
+        toastr.success("The Simulation is Complete. You can now download the employee log files (one after the other).");
+    }
+}
 
 function disableInput() {
 
