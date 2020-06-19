5d88716f108f14082d03bc1a151889477c4a5c24
shawndollar <shawn.dollar@gmail.com>
2020-06-19 02:08:53 -0500

    Add a bit of code to disable input fields and button before submitting.

--- WorkplaceOutbreakSimulatorWebApp/wwwroot/js/simulator.index.js
+++ WorkplaceOutbreakSimulatorWebApp/wwwroot/js/simulator.index.js
@@ -1,4 +1,10 @@
 ﻿// simulator/index js
 $(document).ready(function () {
-
+    
 });
+
+function disableInput() {
+
+    $("main_run_btn").prop("disabled", true);
+    $("#main_run_form :input").not(":button").prop("disabled", true);
+};
